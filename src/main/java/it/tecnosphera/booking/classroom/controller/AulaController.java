ae03be8acdc47a281af6d5acda34810d0a4f5c65
Matteo Avanzini <matteo.a@icanlocalize.com>
2016-06-09 12:11:57 +0200

    Integrato Thymeleaf e Hibernate

--- src/main/java/it/tecnosphera/booking/classroom/controller/AulaController.java
+++ src/main/java/it/tecnosphera/booking/classroom/controller/AulaController.java
@@ -1,17 +1,16 @@
 package it.tecnosphera.booking.classroom.controller;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import it.tecnosphera.booking.classroom.model.Aula;
 
-public class AulaController extends HttpServlet  {
+@Controller
+@RequestMapping(value="/aula")
+public class AulaController  {
 
 	protected Aula loadAulaFromDatabase() {
 		Aula aula = new Aula();
@@ -21,28 +20,18 @@ public class AulaController extends HttpServlet  {
 		return aula;
 	}
 	
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Aula aula = loadAulaFromDatabase();
-		
-		request.setAttribute("aula", aula);
-		String pageContext = request.getContextPath();
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/views/aula/edit.jsp");
-		dispatcher.forward(request, response);
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-    @Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    		Aula aula = new Aula();
-    		aula.setId(Long.parseLong(request.getParameter("id")));
-    		aula.setName(request.getParameter("name"));
-    		aula.setDescription(request.getParameter("description"));
-    		
-    		PrintWriter out = response.getWriter();
-    		RequestDispatcher dispatcher = request.getRequestDispatcher("/views/aula/view.jsp");
-    		dispatcher.forward(request, response);
-	}
+    @RequestMapping(value="/edit/{id}", method = RequestMethod.GET)
+    public String editAula(@PathVariable String id, Model model) {
+    	Aula aula = loadAulaFromDatabase();
+        model.addAttribute("aula", aula);
+        return "aula/edit";
+    }
+  
+    @RequestMapping(value="/view/{id}", method = RequestMethod.GET)
+    public String viewAula(@PathVariable String id, Model model) {
+    	Aula aula = loadAulaFromDatabase();
+        model.addAttribute("aula", aula);
+        return "aula/view";
+    }
+       
 }
