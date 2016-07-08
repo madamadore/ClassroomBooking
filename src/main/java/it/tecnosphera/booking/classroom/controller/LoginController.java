c111331fd1cf4f7e49e9e8470fb30513eefc53ac
Giuseppe Gioffre <ggioffre@tecnosphera.it>
2016-07-08 12:22:23 +0000

    menu_per_admin

--- src/main/java/it/tecnosphera/booking/classroom/controller/LoginController.java
+++ src/main/java/it/tecnosphera/booking/classroom/controller/LoginController.java
@@ -5,7 +5,11 @@ import java.util.Date;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,47 +20,51 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LoginController {
 
 	@RequestMapping(value = "/")
-    public String home(Model m) {
+	public String home(Model m) {
 		Date now = Calendar.getInstance().getTime();
 		m.addAttribute("dataCorrente", now);
-        return "prenotazioni/list";
-    }
-	
-    @RequestMapping(value ="/login")
-    public String login() {
-        return "login";
-    }
-    
-    @RequestMapping(value="/403", method = { RequestMethod.GET, RequestMethod.POST })
-    public @ResponseBody String provideUploadInfo() {
-        return "Access Denied";
-    }
- 
-    // Login form with error
-    @RequestMapping("/login-error")
-    public String loginError(Model model) {
-    	Map<String, Object> map = model.asMap();
-    	for (String index : map.keySet()) {
-    		System.out.println( index + " : " + map.get(index).toString() );
-    	}
-        model.addAttribute("loginError", true);
-        return "login";
-    }
-    
-	 @RequestMapping(value="/logout", method = RequestMethod.GET)  
-	 public String logout(Model model) {  
-		 return "login";  	
-	 }  
-	
-	 @RequestMapping("/error")
-	 public String error(HttpServletRequest request, Model model) {
-		 model.addAttribute("errorCode", request.getAttribute("javax.servlet.error.status_code"));
-	     Throwable throwable = (Throwable) request.getAttribute("javax.servlet.error.exception");
-	     String errorMessage = null;
-	     if (throwable != null) {
-	    	 errorMessage = throwable.getMessage();
-	    	 model.addAttribute("errorMessage", errorMessage.toString());
-	     }
-	     return "error";
-	 }
+		return "prenotazioni/list";
+	}
+
+	@RequestMapping(value = "/login")
+	public String login() {
+		return "login";
+	}
+
+	@RequestMapping(value = "/403", method = { RequestMethod.GET, RequestMethod.POST })
+	public @ResponseBody String provideUploadInfo() {
+		return "Access Denied";
+	}
+
+	// Login form with error
+	@RequestMapping("/login-error")
+	public String loginError(Model model) {
+		Map<String, Object> map = model.asMap();
+		for (String index : map.keySet()) {
+			System.out.println(index + " : " + map.get(index).toString());
+		}
+		model.addAttribute("loginError", true);
+		return "login";
+	}
+
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public String logout(HttpServletRequest request, HttpServletResponse response) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null) {
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		}
+		return "login";
+	}
+
+	@RequestMapping("/error")
+	public String error(HttpServletRequest request, Model model) {
+		model.addAttribute("errorCode", request.getAttribute("javax.servlet.error.status_code"));
+		Throwable throwable = (Throwable) request.getAttribute("javax.servlet.error.exception");
+		String errorMessage = null;
+		if (throwable != null) {
+			errorMessage = throwable.getMessage();
+			model.addAttribute("errorMessage", errorMessage.toString());
+		}
+		return "error";
+	}
 }
